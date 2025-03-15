0baa8426ea1f5e2de58566272abfc762c0b0a3b5
Reza Rezaee <r871017h800501@gmail.com>
2025-03-16 02:39:35 +0330

    complete post model

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,20 @@ from datetime import date
 from django.utils import timezone
 
 
-class BlogPost(models.Model):
+class Post(models.Model):
     day_question = models.CharField(max_length=100)
-    text = models.TextField()
-    publish_date = models.DateField(default=date.today)
-
+    text = models.TextField(default=":( برای این روز هنوز متنی نوشته نشده است")
+    secret_text = models.TextField(null=1, blank=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateField(default=date.today, unique=True)
+    slug = models.SlugField(unique=True)
     is_published = models.BooleanField(default=False)
-    is_developer = models.BooleanField(default=False)
-    is_active    = models.BooleanField(default=False)
+    is_active    = models.BooleanField(default=True)
+
+
+    def publish(self):
+        self.is_published = True
+        self.save()
 
     def should_be_published(self):
         return not self.is_published and self.publish_date <= timezone.now().date()
